9ad0828d0e39cd201fb01d86321dbfa40731fdb5
Daniil Malyshev <m4lyshev@gmail.com>
2026-02-18 18:31:06 +0200

    added test

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,19 @@
+from gendiff import generate_diff
+
+
+def test_generate_diff():
+    file1 = "fixtures/file1.json"
+    file2 = "fixtures/file2.json"
+
+    expected = """{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}"""
+
+    result = generate_diff(file1, file2)
+
+    assert result == expected
